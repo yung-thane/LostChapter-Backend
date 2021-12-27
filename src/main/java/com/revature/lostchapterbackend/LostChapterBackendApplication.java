bf9dcb75775b3167718cf23a3afa921797602c9f
Bach Tran <bach.tran@revature.com>
2021-12-27 15:50:10 -0600

    Initialize Spring Boot project

---
+++ src/main/java/com/revature/lostchapterbackend/LostChapterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.lostchapterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostChapterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostChapterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/lostchapterbackend/LostChapterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.lostchapterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostChapterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
