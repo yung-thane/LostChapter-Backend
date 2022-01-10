405ae977efbed4026ce925d5e613adfbba52a346
marshall-hobbs96 <marshall.hobbs96@gmail.com>
2022-01-10 17:02:19 -0600

    updated book integration tests

--- src/test/java/com/revature/lostchapterbackend/integrationtests/BookIntegrationTest.java
+++ src/test/java/com/revature/lostchapterbackend/integrationtests/BookIntegrationTest.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManagerFactory;
 import com.revature.lostchapterbackend.model.Users;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -447,5 +448,199 @@ public class BookIntegrationTest {
 		this.mvc.perform(builder).andExpect(MockMvcResultMatchers.status().is(400))
 				.andExpect(MockMvcResultMatchers.content().string("Image cannot be blank."));
 	}
+	
+	@Test
+	public void book_controller_test_get_book_by_id_bookNotFoundException_negative() throws Exception {
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books/-1");
+		this.mvc.perform(builder).andExpect(MockMvcResultMatchers.status().is(400)).andExpect(MockMvcResultMatchers.content().string("Book doesn't exist"));
+		
+	}
+	
+	@Test
+	public void book_controller_test_add_book_genre_not_found_exception_negative() throws Exception {
+		
+		AddOrUpdateBookDTO bookToAdd = new AddOrUpdateBookDTO("12345", "bookname5", "synopsis", "author", 12345, 5, 1996, "edition", "publisher", true, 0.5, 10.99, "image");
+		
+		String jsonToSend = mapper.writeValueAsString(bookToAdd);
+		
+		MockHttpSession session = new MockHttpSession(); 
+		
+		session.setAttribute("currentUser", admin);
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/books").session(session)
+				.content(jsonToSend).contentType(MediaType.APPLICATION_JSON);
+		
+		this.mvc.perform(builder).andExpect(MockMvcResultMatchers.status().is(400))
+		.andExpect(MockMvcResultMatchers.content().string("Genre doesn't exist"));
+		
+	}
+	
+	@Test
+	public void book_controller_test_add_book_ISNBAlreadyExistsException_negative() throws Exception {
+		
+		AddOrUpdateBookDTO bookToAdd = new AddOrUpdateBookDTO("1234567879", "bookname5", "synopsis", "author", 1, 5, 1996, "edition", "publisher", true, 0.5, 10.99, "image");
+		
+		String jsonToSend = mapper.writeValueAsString(bookToAdd);
+		
+		MockHttpSession session = new MockHttpSession(); 
+		
+		session.setAttribute("currentUser", admin);
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/books").session(session)
+				.content(jsonToSend).contentType(MediaType.APPLICATION_JSON);
+		
+		this.mvc.perform(builder).andExpect(MockMvcResultMatchers.status().is(400))
+		.andExpect(MockMvcResultMatchers.content().string("ISBN already used for another book"));
+		
+	}
+	
+	@Test
+	public void book_controller_successfully_updating_book_positive() throws Exception {
+		
+		AddOrUpdateBookDTO bookToSend = new AddOrUpdateBookDTO("000001", "bookName", "synopsis",
+				"author", 1, 5, 1996, "edition",
+				"publisher", true,
+				0.5, 10.99, "image");
+		
+		String jsonToSend = mapper.writeValueAsString(bookToSend);
+		
+		Book actualBook = new Book("000001", "bookName", "synopsis",
+				"author", g, 5, 1996, "edition",
+				"publisher", true,
+				0.5, 10.99, "image");
+		
+		actualBook.setBookId(1);
+		
+		String expectedJson = mapper.writeValueAsString(actualBook);
+		
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute("currentUser", admin);
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/books/1").session(session).content(jsonToSend).contentType(MediaType.APPLICATION_JSON);
+		
+		this.mvc.perform(builder).andExpect(MockMvcResultMatchers.status().is(200)).andExpect(MockMvcResultMatchers.content().json(expectedJson));
+		
+	}
+	
+	@Test
+	public void book_controller_update_book_BookNotFoundException_negative() throws Exception {
+		
+		AddOrUpdateBookDTO actualBook = new AddOrUpdateBookDTO("000001", "bookName", "synopsis",
+				"author", 1, 5, 1996, "edition",
+				"publisher", true,
+				0.5, 10.99, "image");
+		
+		String jsonToSend = mapper.writeValueAsString(actualBook);
+		
+		
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute("currentUser", admin);
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/books/20").session(session).content(jsonToSend).contentType(MediaType.APPLICATION_JSON);
+		
+		this.mvc.perform(builder).andExpect(MockMvcResultMatchers.status().is(400)).andExpect(MockMvcResultMatchers.content().string("Book doesn't exist"));
+		
+	}
+	
+	@Test
+	public void book_controller_update_book_InvalidParamterException_negative() throws Exception {
+		
+		AddOrUpdateBookDTO actualBook = new AddOrUpdateBookDTO("000001", "bookName", "synopsis",
+				"author", 1, 5, 1996, "edition",
+				"publisher", true,
+				0.5, 10.99, "image");
+		
+		String jsonToSend = mapper.writeValueAsString(actualBook);
+		
+		
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute("currentUser", admin);
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/books/a").session(session).content(jsonToSend).contentType(MediaType.APPLICATION_JSON);
+		
+		this.mvc.perform(builder).andExpect(MockMvcResultMatchers.status().is(400)).andExpect(MockMvcResultMatchers.content().string("Id must be in Int format"));
+		
+	}
+	
+	@Test
+	public void book_controller_update_book_GenreNotFoundException_negative() throws Exception {
+		
+		AddOrUpdateBookDTO actualBook = new AddOrUpdateBookDTO("000001", "bookName", "synopsis",
+				"author", 20, 5, 1996, "edition",
+				"publisher", true,
+				0.5, 10.99, "image");
+		
+		String jsonToSend = mapper.writeValueAsString(actualBook);
+		
+		
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute("currentUser", admin);
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/books/1").session(session).content(jsonToSend).contentType(MediaType.APPLICATION_JSON);
+		
+		this.mvc.perform(builder).andExpect(MockMvcResultMatchers.status().is(400)).andExpect(MockMvcResultMatchers.content().string("Genre doesn't exist"));
+		
+	}
+	
+	@Test
+	public void book_controller_update_book_SaleDiscountRateException_negative() throws Exception {
+		
+		AddOrUpdateBookDTO actualBook = new AddOrUpdateBookDTO("000001", "bookName", "synopsis",
+				"author", 1, 5, 1996, "edition",
+				"publisher", true,
+				1, 10.99, "image");
+		
+		String jsonToSend = mapper.writeValueAsString(actualBook);
+		
+		
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute("currentUser", admin);
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/books/1").session(session).content(jsonToSend).contentType(MediaType.APPLICATION_JSON);
+		
+		this.mvc.perform(builder).andExpect(MockMvcResultMatchers.status().is(400)).andExpect(MockMvcResultMatchers.content().string("Sale discount rate must be more than 0.0 and less than or equal to 0.90"));
+		
+	}
+	
+	@Test
+	public void book_controller_update_book_ISNBAlreadyExistsException_negative() throws Exception {
+		
+		AddOrUpdateBookDTO actualBook = new AddOrUpdateBookDTO("2122232425", "bookName", "synopsis",
+				"author", 1, 5, 1996, "edition",
+				"publisher", true,
+				0.5, 10.99, "image");
+		
+		String jsonToSend = mapper.writeValueAsString(actualBook);
+		
+		
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute("currentUser", admin);
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/books/1").session(session).content(jsonToSend).contentType(MediaType.APPLICATION_JSON);
+		
+		this.mvc.perform(builder).andExpect(MockMvcResultMatchers.status().is(400)).andExpect(MockMvcResultMatchers.content().string("ISBN already used for another book"));
+		
+	}
+	
+	@Test
+	public void book_controller_get_book_by_id_InvalidParamterException_negative() throws Exception {	//Yall didnt catch this exception. Thrown in service layer, but not caught in controller layer.
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books/a"); 
+		this.mvc.perform(builder).andExpect(MockMvcResultMatchers.status().is(400)).andExpect(MockMvcResultMatchers.content().string("The Id entered must be an int."));
+		
+	}
+	
+	@Test
+	public void book_controller_get_books_by_genre_id_InvalidParamterException_negative() throws Exception { //Same thing here
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books/genre/a");
+		this.mvc.perform(builder).andExpect(MockMvcResultMatchers.status().is(400)).andExpect(MockMvcResultMatchers.content().string("The Id entered must be an int."));
+		
+	}
+	
+	
+	
+
 
 }
